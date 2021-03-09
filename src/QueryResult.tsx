f7ba4904fea5e06f8c6621f805c58e1bdfe10ee6
Chiawen Chen <golopot@gmail.com>
2021-03-09 17:17:03 +0800

    clean up

--- src/QueryResult.tsx
+++ src/QueryResult.tsx
@@ -1,27 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import * as Connector from './connector';
 import { QueryResult as Result } from './driver/driver';
 import ReactDataGrid from 'react-data-grid';
-
-// const columns = ['name', 'telephone', 'location'];
-
-// const rows = [
-//   ['Maria Garcia', '(442) 094-8134', 'Vermont'],
-//   ['Maria Garcia', '(442) 094-8134', 'Vermont'],
-//   ['Maria Garcia', '(442) 094-8134', 'Vermont'],
-//   ['Maria Garcia', '(442) 094-8134', 'Vermont'],
-// ];
-
-const STable = styled.table`
-  border-collapse: collapse;
-  td,
-  th {
-    min-width: 80px;
-    padding: 4px 0 4px 2px;
-    border: 1px black solid;
-  }
-`;
 
 export default function QueryResult({
   connector,
